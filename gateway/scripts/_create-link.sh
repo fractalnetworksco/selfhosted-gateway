cc8e20bf956e337c8fa5db220fdea41714e213df
Mo Balaa <balaa@ether.ai>
2022-09-20 22:40:20 -0400

    replace string source with array access

--- gateway/scripts/_create-link.sh
+++ gateway/scripts/_create-link.sh
@@ -14,6 +14,5 @@ WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.
 # get public ipv4 address
 GATEWAY_IP=$(curl -s 4.icanhazip.com)
 
-echo "export GATEWAY_LINK_WG_PUBKEY=$GATEWAY_LINK_WG_PUBKEY"
-echo "export GATEWAY_ENDPOINT=$GATEWAY_IP:$WIREGUARD_PORT"
+echo "$GATEWAY_LINK_WG_PUBKEY $GATEWAY_IP:$WIREGUARD_PORT"
 

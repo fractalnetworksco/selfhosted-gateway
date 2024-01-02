4449b79b9b01b82b63dbc27c92c06c4d1bb49fb8
Dotsch2005 <3149448+Dotsch2005@users.noreply.github.com>
2024-01-02 19:04:32 +0000

    implementiert static port for restart and add insecure function https backend with self-signed certificate

--- src/create-link/remote.sh
+++ src/create-link/remote.sh
@@ -7,10 +7,18 @@ LINK_CLIENT_WG_PUBKEY=$2
 
 # create gateway-link container
 CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p 18521/udp --cap-add NET_ADMIN --restart unless-stopped -it -e LINK_CLIENT_WG_PUBKEY=$LINK_CLIENT_WG_PUBKEY -d fractalnetworks/gateway-link:latest)
+# get randomly assigned WireGuard port
+WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.0\.0://")
+
+docker stop $CONTAINER_ID 2>& 1>NUL
+docker rm $CONTAINER_ID 2>& 1>NUL
+
+# create gateway-link container
+CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p $WIREGUARD_PORT:18521/udp --cap-add NET_ADMIN --restart unless-stopped -it -e LINK_CLIENT_WG_PUBKEY=$LINK_CLIENT_WG_PUBKEY -d fractalnetworks/gateway-link:latest)
 # get gateway-link WireGuard pubkey 
 GATEWAY_LINK_WG_PUBKEY=$(docker exec $CONTAINER_NAME bash -c 'cat /etc/wireguard/link0.key |wg pubkey')
 # get randomly assigned WireGuard port
-WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.0\.0://")
+#WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.0\.0://")
 # get public ipv4 address
 GATEWAY_IP=$(curl -s 4.icanhazip.com)
 

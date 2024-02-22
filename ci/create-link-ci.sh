17e28aba770392b2681bff7ab81baf35401ec421
Mo Balaa <balaa@fractalnetworks.co>
2024-02-22 15:45:16 -0600

    fix path

--- ci/create-link-ci.sh
+++ ci/create-link-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 eval $(ssh-agent -s)
-ssh-add ./gateway-sim-key
+ssh-add ./ci/gateway-sim-key
 docker run --network gateway -e SSH_AGENT_PID=$SSH_AGENT_PID -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK --rm fractalnetworks/gateway-cli:latest $1 $2 $3

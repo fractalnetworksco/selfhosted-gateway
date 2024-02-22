46316d576fac0c51af50ee29a6a5e169707ef231
Mo Balaa <balaa@fractalnetworks.co>
2024-02-22 15:57:54 -0600

    teardown in ci file

--- ci/create-link-ci.sh
+++ ci/create-link-ci.sh
@@ -3,8 +3,9 @@ set -e
 
 cd ci/
 ssh-keygen -t ed25519 -f ./gateway-sim-key -N ""
+# create docker network if not exists
+docker network create gateway || true
 docker compose up -d --build
 eval $(ssh-agent -s)
 ssh-add ./gateway-sim-key
 docker run --network gateway -e SSH_AGENT_PID=$SSH_AGENT_PID -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK --rm fractalnetworks/gateway-cli:latest $1 $2 $3
-docker compose down

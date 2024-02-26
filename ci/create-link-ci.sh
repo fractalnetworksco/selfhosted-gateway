09fcfd858c6ec24855c37c01ba93ee37da14eda5
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-02-26 17:32:12 -0600

    caddy TLS test v1.1

--- ci/create-link-ci.sh
+++ ci/create-link-ci.sh
@@ -28,24 +28,38 @@ if ! docker compose exec gateway curl -k -H "Authorization: Basic YWRtaW46YWRtaW
 fi
 docker compose -f test-link.yaml down
 docker rm -f app-example-com
-# rm test-link.yaml                 # not removing for now...
+rm test-link.yaml                 # comment out to keep the file for debugging
 
-caddy_greenlight=false               # andrew's sentinel thing
+
+caddy_greenlight=true               # andrew's sentinel thing
 
 if [ "$caddy_greenlight" = true ]; then
+    echo "*******************Testing Caddy TLS Proxy"
     # Test the link using  CADDY_TLS_PROXY: true
-    testLinkFile="test-link-caddyTLS.yaml"
-
     # generate new docker compose
+    testLinkFile="test-link-caddyTLS.yaml"
     cat test-link.template.yaml > $testLinkFile
     docker run --network gateway -e SSH_AGENT_PID=$SSH_AGENT_PID -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK --rm fractalnetworks/gateway-cli:latest $1 $2 $3 >> $testLinkFile
     cat network.yaml >> $testLinkFile
-    # set the gateway endpoint to the gateway link container
+
+    # Go inside $testLinkFile and change...
+    # gateway endpoint to the gateway link container
     sed -i 's/^\(\s*GATEWAY_ENDPOINT:\).*/\1 app-example-com:18521/' $testLinkFile
+
+    # set CADDY_TLS_PROXY to true
+    sed -i 's/^\(\s*\)#\s*CADDY_TLS_PROXY: true/\1CADDY_TLS_PROXY: true/' $testLinkFile
 
     docker compose -f $testLinkFile up -d
     docker compose -f $testLinkFile exec link ping 10.0.0.1 -c 2
+    # assert http response code was 200
+    # asserts basic auth is working with user: admin, password: admin
 
+    if ! docker compose exec gateway curl -k -H "Authorization: Basic YWRtaW46YWRtaW4=" --resolve app.example.com:443:127.0.0.1 https://app.example.com -I |grep "HTTP/2 200"; then
+        FAILED="true"
+    fi
+    docker compose -f $testLinkFile down
+    docker rm -f app-example-com
+    rm $testLinkFile                 # comment out to keep the file for debugging
 fi
 
 

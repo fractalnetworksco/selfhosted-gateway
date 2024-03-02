cbd1a66d4bfb27896ccd68abae70e35826e567d5
Ryan Persée <98691129+rpersee@users.noreply.github.com>
2024-03-02 11:36:03 +0100

    Handle Gateway in a private subnet

--- src/create-link/remote.sh
+++ src/create-link/remote.sh
@@ -19,8 +19,6 @@ CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p $WIREGUARD
 GATEWAY_LINK_WG_PUBKEY=$(docker exec $CONTAINER_NAME bash -c 'cat /etc/wireguard/link0.key |wg pubkey')
 # get randomly assigned WireGuard port
 #WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.0\.0://")
-# get public ipv4 address
-GATEWAY_IP=$(curl -s 4.icanhazip.com)
 
-echo "$GATEWAY_LINK_WG_PUBKEY $GATEWAY_IP:$WIREGUARD_PORT"
+echo "$GATEWAY_LINK_WG_PUBKEY $WIREGUARD_PORT"
 

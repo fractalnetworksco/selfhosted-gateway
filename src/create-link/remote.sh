159132df162ed5220e0f6be55edbb6c05c082643
Zach Lott <zacharylott94@gmail.com>
2024-04-01 12:11:02 -0500

    WIP

--- src/create-link/remote.sh
+++ src/create-link/remote.sh
@@ -4,6 +4,8 @@ set -e
 
 CONTAINER_NAME=$1
 LINK_CLIENT_WG_PUBKEY=$2
+FORWARD_PORT=$3
+FORWARD_PROTOCOL=$4
 
 # create gateway-link container
 CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p 18521/udp -p 25565/tcp --cap-add NET_ADMIN --restart unless-stopped -it -e LINK_CLIENT_WG_PUBKEY=$LINK_CLIENT_WG_PUBKEY -d fractalnetworks/gateway-link:latest)
@@ -13,7 +15,7 @@ WIREGUARD_PORT=$(docker port $CONTAINER_NAME 18521/udp| head -n 1| sed "s/0\.0\.
 docker rm -f $CONTAINER_ID 2>& 1>NUL
 
 # create gateway-link container
-CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p $WIREGUARD_PORT:18521/udp --cap-add NET_ADMIN --restart unless-stopped -it -e LINK_CLIENT_WG_PUBKEY=$LINK_CLIENT_WG_PUBKEY -d fractalnetworks/gateway-link:latest)
+CONTAINER_ID=$(docker run --name $CONTAINER_NAME --network gateway -p $WIREGUARD_PORT:18521/udp -p $FORWARD_PORT:$FORWARD_PORT/$FORWARD_PROTOCOL --cap-add NET_ADMIN --restart unless-stopped -it -e LINK_CLIENT_WG_PUBKEY=$LINK_CLIENT_WG_PUBKEY -d fractalnetworks/gateway-link:latest)
 # get gateway-link WireGuard pubkey 
 GATEWAY_LINK_WG_PUBKEY=$(docker exec $CONTAINER_NAME bash -c 'cat /etc/wireguard/link0.key |wg pubkey')
 # get randomly assigned WireGuard port

de2c340c6879251674db286fa917d048026b21c6
Mo Balaa <balaa@ether.ai>
2024-06-17 15:17:25 -0500

    Fix it

--- src/client-link/entrypoint.sh
+++ src/client-link/entrypoint.sh
@@ -94,8 +94,8 @@ else
         # Just opening both TCP and UDP is the quick and dirty way of ensuring both protocols work
         # In the future, specifying a protocol in the docker compose snippet may be necessary
         # -- 2024-04-07 zacharylott94@gmail.com
-        socat TCP4-LISTEN:$CENTER_PORT,fork,reuseaddr TCP4:$EXPOSE,reuseaddr,fork &
-        socat UDP4-LISTEN:$CENTER_PORT,fork,reuseaddr UDP4:$EXPOSE,reuseaddr,fork 
+        socat TCP4-LISTEN:$CENTER_PORT,fork,reuseaddr TCP4:$EXPOSE,reuseaddr&
+        socat UDP4-LISTEN:$CENTER_PORT,fork,reuseaddr UDP4:$EXPOSE,reuseaddr 
     else
         echo "Caddy is disabled. Using socat to forward traffic to app."
         socat TCP4-LISTEN:8080,fork,reuseaddr TCP4:$EXPOSE,reuseaddr &
